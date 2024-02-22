2d2cbf955c74700d274ea7434e96fd1e666baf4b
pramodsaini <pramodsaini25062000@gmail.com>
2024-02-22 03:01:46 -0800

    insert record into database

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,24 +2,24 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
+      
+	 public function __construct(){
+		parent:: __construct();
+		$this->load->helper("form");
+		$this->load->dataBase();
+	 }
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+	public function submit(){
+		 $data=array(
+			"sn"=>$this->input->post("roll"),
+			"name"=>$this->input->post("user"),
+			"avgr"=>$this->input->post("avg")
+		 );
+		 $this->db->insert("demo",$data);
+		 $msg["message"]="Record Inserted";
+		 $this->load->view("Welcome_message",$msg);
 	}
 }
